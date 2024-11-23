e0511c31bdbd8f3997253cb8a617abe21e5f9ba9
shuiping233 <1944680304@qq.com>
2024-11-23 19:38:48 +0800

    重制自动归档流水线

--- rn_issues_auto_archiving/src/shared/ci_event_type.py
+++
@@ -1,7 +0,0 @@
-
-class CiEventType():
-    '''流水线触发类型 :\n
-    github:https://docs.github.com/zh/actions/writing-workflows/choosing-when-your-workflow-runs/events-that-trigger-workflows#about-events-that-trigger-workflows
-    gitlab:https://docs.gitlab.com/ee/ci/jobs/job_rules.html#ci_pipeline_source-predefined-variable'''
-    manual = ["web", "workflow_dispatch"]
-    issue_event = ["trigger", "issues"]
